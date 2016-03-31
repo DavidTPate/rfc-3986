6c0029119afcc7aa4c37a47c959b020ec4f65fca
David Pate <davidtpate@gmail.com>
2016-03-30 22:14:38 -0400

    Initial version.

---
+++ test/rfc3986.js
@@ -0,0 +1,25 @@
+'use strict';
+var assert = require('assert');
+
+var rfc3986 = require('../index');
+
+describe('RFC-3986', function () {
+
+    var propertiesToExpose = [
+        'cidr',
+        'IPv4address',
+        'IPv6address',
+        'IPvFuture',
+        'IPLiteral',
+        'scheme',
+        'hierPart',
+        'query',
+        'fragment',
+        'uri'
+    ];
+    propertiesToExpose.forEach(function (prop) {
+        it('should expose ' + prop, function () {
+            assert.ok(rfc3986[prop]);
+        });
+    });
+});
